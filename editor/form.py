1577c9838541b0cc21c0e8267c083f13eb4257df
HeyD <hmhati@gmail.com>
2026-08-22 11:30:02 +0300

    Multi dictionary support

--- editor/form.py
+++ editor/form.py
@@ -1,10 +1,12 @@
 from aqt.qt import (
-    QLocale,
     Qt,
     QCoreApplication,
     QMetaObject,
-    QRect,
     QUrl,
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+    QGroupBox,
     QWidget,
     QLabel,
     QCheckBox,
@@ -15,8 +17,7 @@ from aqt.qt import (
     QTabWidget,
     QWebEngineView,
     QWebEngineSettings,
-    QFrame,
-    QFont,
+    QStyle,
 )
 from aqt import mw
 from aqt.theme import colors
@@ -73,7 +74,7 @@ class DefinitionWebView(QWebEngineView):
                     width: 12vmin;
                     background-color: transparent;
                 }}
-                ::webkit-scrollbar-track {{
+                ::-webkit-scrollbar-track {{
                     background-color: transparent;
                 }}
                 ::-webkit-scrollbar-thumb {{
@@ -108,202 +109,151 @@ class DefinitionWebView(QWebEngineView):
 class Ui_Dialog(object):
     def setupUi(self, Dialog: QWidget):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(461, 481)
+        Dialog.setMinimumSize(461, 481)
         Dialog.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.start = QPushButton(parent=Dialog)
-        self.start.setGeometry(QRect(10, 450, 441, 23))
-        self.start.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.start.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
+
+        main_layout = QVBoxLayout(Dialog)
+
+        self.dict_group = QGroupBox(parent=Dialog)
+        self.dict_group.setObjectName("dict_group")
+        dict_layout = QVBoxLayout(self.dict_group)
+
+        dict_buttons_row = QHBoxLayout()
+        self.dictionary = QComboBox(parent=self.dict_group)
+        self.dictionary.setObjectName("dictionary")
+        self.dictionary.setSizePolicy(
+            self.dictionary.sizePolicy().horizontalPolicy(),
+            self.dictionary.sizePolicy().verticalPolicy(),
         )
-        self.start.setObjectName("start")
-        self.label_3 = QLabel(parent=Dialog)
-        self.label_3.setGeometry(QRect(20, 350, 131, 21))
-        self.label_3.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.label_3.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_3.setObjectName("label_3")
-        self.label_4 = QLabel(parent=Dialog)
-        self.label_4.setGeometry(QRect(20, 380, 131, 21))
-        self.label_4.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.label_4.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_4.setObjectName("label_4")
-        self.destination_field = QComboBox(parent=Dialog)
-        self.destination_field.setGeometry(QRect(160, 380, 281, 22))
-        self.destination_field.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.destination_field.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.destination_field.setObjectName("destination_field")
-        self.note_type = QComboBox(parent=Dialog)
-        self.note_type.setGeometry(QRect(160, 320, 281, 22))
-        self.note_type.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.note_type.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.note_type.setObjectName("note_type")
-        self.line_2 = QFrame(parent=Dialog)
-        self.line_2.setGeometry(QRect(20, 300, 421, 20))
-        font = QFont()
-        font.setBold(True)
-        font.setWeight(75)
-        self.line_2.setFont(font)
-        self.line_2.setStyleSheet("border-color: red;")
-        self.line_2.setFrameShape(QFrame.Shape.HLine)
-        self.line_2.setFrameShadow(QFrame.Shadow.Sunken)
-        self.line_2.setObjectName("line_2")
-        self.label_2 = QLabel(parent=Dialog)
-        self.label_2.setGeometry(QRect(20, 320, 131, 21))
-        self.label_2.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.label_2.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_2.setObjectName("label_2")
-        self.overwrite_destination = QCheckBox(parent=Dialog)
-        self.overwrite_destination.setGeometry(QRect(160, 410, 81, 21))
-        self.overwrite_destination.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.overwrite_destination.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.overwrite_destination.setText("")
-        self.overwrite_destination.setObjectName("overwrite_destination")
-        self.label_6 = QLabel(parent=Dialog)
-        self.label_6.setGeometry(QRect(20, 280, 191, 31))
-        font = QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_6.setFont(font)
-        self.label_6.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_6.setObjectName("label_6")
-        self.source_field = QComboBox(parent=Dialog)
-        self.source_field.setGeometry(QRect(160, 350, 281, 22))
-        self.source_field.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.source_field.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.source_field.setObjectName("source_field")
-        self.label_5 = QLabel(parent=Dialog)
-        self.label_5.setGeometry(QRect(20, 410, 131, 21))
-        self.label_5.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.label_5.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_5.setObjectName("label_5")
-        self.search = QPushButton(parent=Dialog)
-        self.search.setGeometry(QRect(320, 130, 121, 23))
-        self.search.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.search.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.search.setObjectName("search")
-        self.tabWidget = QTabWidget(parent=Dialog)
-        self.tabWidget.setGeometry(QRect(20, 90, 281, 181))
+        dict_buttons_row.addWidget(self.dictionary, 1)
+
+        self.dict_prev = QPushButton(parent=self.dict_group)
+        self.dict_prev.setObjectName("dict_prev")
+        self.dict_prev.setIcon(self.dict_prev.style().standardIcon(QStyle.StandardPixmap.SP_ArrowLeft))
+        self.dict_prev.setFixedWidth(28)
+        dict_buttons_row.addWidget(self.dict_prev)
+
+        self.dict_next = QPushButton(parent=self.dict_group)
+        self.dict_next.setObjectName("dict_next")
+        self.dict_next.setIcon(self.dict_next.style().standardIcon(QStyle.StandardPixmap.SP_ArrowRight))
+        self.dict_next.setFixedWidth(28)
+        dict_buttons_row.addWidget(self.dict_next)
+
+        self.browse = QPushButton(parent=self.dict_group)
+        self.browse.setObjectName("browse")
+        dict_buttons_row.addWidget(self.browse)
+
+        dict_layout.addLayout(dict_buttons_row)
+
+        dict_content_row = QHBoxLayout()
+
+        self.tabWidget = QTabWidget(parent=self.dict_group)
         self.tabWidget.setFocusPolicy(Qt.FocusPolicy.NoFocus)
         self.tabWidget.setObjectName("tabWidget")
+
         self.tab_1 = QWidget()
         self.tab_1.setObjectName("tab_1")
+        tab1_layout = QVBoxLayout(self.tab_1)
+        tab1_layout.setContentsMargins(0, 0, 0, 0)
         self.definition_preview = DefinitionWebView(parent=self.tab_1)
-        self.definition_preview.setGeometry(QRect(-1, -1, 281, 151))
         self.definition_preview.setAutoFillBackground(True)
-        self.definition_preview.setStyleSheet("QTextEdit {\n    background: red;\n}")
         self.definition_preview.setObjectName("definition_preview")
+        tab1_layout.addWidget(self.definition_preview)
         self.tabWidget.addTab(self.tab_1, "")
+
         self.tab_2 = QWidget()
         self.tab_2.setObjectName("tab_2")
+        tab2_layout = QVBoxLayout(self.tab_2)
+        tab2_layout.setContentsMargins(0, 0, 0, 0)
         self.definition_source = QTextEdit(parent=self.tab_2)
-        self.definition_source.setGeometry(QRect(0, 0, 281, 151))
-        self.definition_source.setStyleSheet("")
         self.definition_source.setReadOnly(True)
         self.definition_source.setObjectName("definition_source")
+        tab2_layout.addWidget(self.definition_source)
         self.tabWidget.addTab(self.tab_2, "")
-        self.dictionary_title = QLineEdit(parent=Dialog)
-        self.dictionary_title.setGeometry(QRect(20, 50, 281, 21))
-        self.dictionary_title.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.dictionary_title.setAcceptDrops(True)
-        self.dictionary_title.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.dictionary_title.setText("")
-        self.dictionary_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.dictionary_title.setReadOnly(True)
-        self.dictionary_title.setObjectName("dictionary_title")
-        self.text_format = QComboBox(parent=Dialog)
-        self.text_format.setGeometry(QRect(320, 250, 121, 21))
-        self.text_format.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.text_format.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.text_format.setObjectName("text_format")
-        self.line = QFrame(parent=Dialog)
-        self.line.setGeometry(QRect(20, 30, 421, 20))
-        self.line.setFrameShape(QFrame.Shape.HLine)
-        self.line.setFrameShadow(QFrame.Shadow.Sunken)
-        self.line.setObjectName("line")
-        self.browse = QPushButton(parent=Dialog)
-        self.browse.setGeometry(QRect(320, 50, 121, 23))
-        self.browse.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        self.browse.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.browse.setObjectName("browse")
-        self.label_7 = QLabel(parent=Dialog)
-        self.label_7.setGeometry(QRect(320, 220, 121, 21))
-        self.label_7.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.label_7.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label_7.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label_7.setObjectName("label_7")
-        self.label = QLabel(parent=Dialog)
-        self.label.setGeometry(QRect(20, 10, 191, 31))
-        font = QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse
-            | Qt.TextInteractionFlag.TextSelectableByMouse
-        )
-        self.label.setObjectName("label")
-        self.word = QLineEdit(parent=Dialog)
-        self.word.setGeometry(QRect(320, 100, 121, 21))
-        self.word.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.word.setLocale(
-            QLocale(QLocale.Language.English, QLocale.Country.UnitedStates)
-        )
-        self.word.setText("")
+
+        dict_content_row.addWidget(self.tabWidget, 1)
+
+        right_column = QVBoxLayout()
+
+        self.word = QLineEdit(parent=self.dict_group)
+        self.word.setObjectName("word")
         self.word.setMaxLength(30)
         self.word.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.word.setObjectName("word")
+        right_column.addWidget(self.word)
+
+        self.search = QPushButton(parent=self.dict_group)
+        self.search.setObjectName("search")
+        right_column.addWidget(self.search)
+
+        right_column.addStretch()
+
+        self.label_7 = QLabel(parent=self.dict_group)
+        self.label_7.setObjectName("label_7")
+        self.label_7.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        right_column.addWidget(self.label_7)
+
+        self.text_format = QComboBox(parent=self.dict_group)
+        self.text_format.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.text_format.setObjectName("text_format")
+        right_column.addWidget(self.text_format)
+
+        dict_content_row.addLayout(right_column)
+
+        dict_layout.addLayout(dict_content_row)
+
+        main_layout.addWidget(self.dict_group)
+
+        self.note_group = QGroupBox(parent=Dialog)
+        self.note_group.setObjectName("note_group")
+        note_layout = QGridLayout(self.note_group)
+
+        self.label_2 = QLabel(parent=self.note_group)
+        self.label_2.setObjectName("label_2")
+        note_layout.addWidget(self.label_2, 0, 0)
+        self.note_type = QComboBox(parent=self.note_group)
+        self.note_type.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.note_type.setObjectName("note_type")
+        note_layout.addWidget(self.note_type, 0, 1)
+
+        self.label_3 = QLabel(parent=self.note_group)
+        self.label_3.setObjectName("label_3")
+        note_layout.addWidget(self.label_3, 1, 0)
+        self.source_field = QComboBox(parent=self.note_group)
+        self.source_field.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.source_field.setObjectName("source_field")
+        note_layout.addWidget(self.source_field, 1, 1)
+
+        self.label_4 = QLabel(parent=self.note_group)
+        self.label_4.setObjectName("label_4")
+        note_layout.addWidget(self.label_4, 2, 0)
+        self.destination_field = QComboBox(parent=self.note_group)
+        self.destination_field.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.destination_field.setObjectName("destination_field")
+        note_layout.addWidget(self.destination_field, 2, 1)
+
+        overwrite_row = QHBoxLayout()
+        self.label_5 = QLabel(parent=self.note_group)
+        self.label_5.setObjectName("label_5")
+        overwrite_row.addWidget(self.label_5)
+        self.overwrite_destination = QCheckBox(parent=self.note_group)
+        self.overwrite_destination.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.overwrite_destination.setText("")
+        self.overwrite_destination.setObjectName("overwrite_destination")
+        overwrite_row.addWidget(self.overwrite_destination)
+        overwrite_row.addStretch()
+        note_layout.addLayout(overwrite_row, 3, 0, 1, 2)
+
+        main_layout.addWidget(self.note_group)
+
+        self.start = QPushButton(parent=Dialog)
+        self.start.setObjectName("start")
+        main_layout.addWidget(self.start)
 
         self.definition_preview.settings().setAttribute(
             QWebEngineSettings.WebAttribute.JavascriptEnabled, False
         )
         self.retranslateUi(Dialog)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
@@ -313,7 +263,6 @@ class Ui_Dialog(object):
         self.label_3.setText(_translate("Dialog", "Source field"))
         self.label_4.setText(_translate("Dialog", "Destination field"))
         self.label_2.setText(_translate("Dialog", "Note type"))
-        self.label_6.setText(_translate("Dialog", "Note options"))
         self.label_5.setText(_translate("Dialog", "Overwrite destination"))
         self.search.setText(_translate("Dialog", "Search"))
         self.tabWidget.setTabText(
@@ -328,10 +277,11 @@ class Ui_Dialog(object):
         self.tabWidget.setTabText(
             self.tabWidget.indexOf(self.tab_2), _translate("Dialog", "Source")
         )
-        self.dictionary_title.setPlaceholderText(
-            _translate("Dialog", "Select your dictionary")
-        )
         self.browse.setText(_translate("Dialog", "Browse"))
         self.label_7.setText(_translate("Dialog", "Text format"))
-        self.label.setText(_translate("Dialog", "Dictionary options"))
+        self.dict_group.setTitle(_translate("Dialog", "Dictionary options"))
+        self.note_group.setTitle(_translate("Dialog", "Note options"))
         self.word.setPlaceholderText(_translate("Dialog", "Enter a word"))
+        self.dictionary.setPlaceholderText(
+            _translate("Dialog", "Select your dictionary")
+        )
